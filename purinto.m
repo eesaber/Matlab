7366be153f4ecd84dd31c294f84ab78573bd31da
Torres <fbookzone@gmail.com>
2016-12-07 18:33:53 +0800

    Colorbar, from red to blue

--- purinto.m
+++ purinto.m
@@ -2,12 +2,13 @@ function purinto(s)
 print = true ;
 if(print) 
 %%It will print the magnitude of the input signal 
-    figure
-    imagesc(abs(s))
-    set(gca,'Ydir','normal')   
-    xlabel('Range $(\Delta \tau)$','Interpreter','latex')
-    ylabel('Azimuth $(\Delta \eta)$','Interpreter','latex')
-    set(gca,'FontSize',26,'Fontname','CMU Serif')
-    colorbar
-    pbaspect([4 3 1])
+	figure
+	imagesc(abs(s))
+	set(gca,'Ydir','normal')   
+	xlabel('Range $(\Delta \tau)$','Interpreter','latex')
+	ylabel('Azimuth $(\Delta \eta)$','Interpreter','latex')
+	set(gca,'FontSize',26,'Fontname','CMU Serif')
+	colormap('default')
+	colormap('Jet')
+	pbaspect([4 3 1])
 end
