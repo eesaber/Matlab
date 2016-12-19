8422631d95d1391a4adf8fb1241d45ad0d251598
EE527 <r05942013@ntu.edu.tw>
2016-12-19 17:59:22 +0800

    Check run_sar and my_plot work correctly

--- run_sar.m
+++ run_sar.m
@@ -1,18 +1,32 @@
-clear
-over = 4;
+over = 6;
 step = 3;
-x = linsapce(0, over * step, step); 
+x = 0: step: (over - 1) * step ; 
 %% v_y = 0 , v_x change
 for kk = 1 : over
-  [r_PLSR(kk), a_PLSR(k), rsandb(kk), asandb(kk), t_v_x(kk), t_v_y(kk)] =  SAR((kk - 1) * step, 0);
+  [r_PLSR(kk), a_PLSR(kk), rsandb(kk), asandb(kk), t_v_x(kk), t_v_y(kk)] =  SAR((kk - 1) * step, 0);
 end
-figure 
-subplot
+my_plot(t_v_x - x,  x, '$\tilde{v}_x - v_x$')
+my_plot(r_PLSR, x, 'Range PLSR')
+my_plot(a_PLSR, x, 'Azimuth PLSR')
+my_plot(rsandb, x, 'Range pulse width')
+my_plot(asandb, x, 'Azimuth pulse width')
+
 %% v_y change, v_x = 0
 for kk = 1 : over
-   SAR(0, kk * step)
+   SAR(0, (kk-1) * step)
 end
+my_plot(t_v_y - x,  x, '$\tilde{v}_y - v_y$')
+my_plot(r_PLSR, x, 'Range PLSR')
+my_plot(a_PLSR, x, 'Azimuth PLSR')
+my_plot(rsandb, x, 'Range pulse width')
+my_plot(asandb, x, 'Azimuth pulse width')
 %% v_y = v_x 
 for kk = 1 : over
-   SAR(kk * step, kk * step)
+   SAR((kk-1) * step, (kk-1) * step)
 end
+my_plot(t_v_x - x,  x, '$\tilde{v}_x - v_x$')
+my_plot(t_v_y - x,  x, '$\tilde{v}_y - v_y$')
+my_plot(r_PLSR, x, 'Range PLSR')
+my_plot(a_PLSR, x, 'Azimuth PLSR')
+my_plot(rsandb, x, 'Range pulse width')
+my_plot(asandb, x, 'Azimuth pulse width')
