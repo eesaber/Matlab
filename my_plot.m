d224693a215b5978dbd860539b09810beac449cc
EE527 <r05942013@ntu.edu.tw>
2016-12-22 14:07:13 +0800

    My_plot: change font size, and can input to signal

--- my_plot.m
+++ my_plot.m
@@ -1,15 +1,24 @@
-function my_plot(s, x_s, y_la, ti)
-print = true ;
-if(print) 
+function my_plot(s1, s2, x_s, y_la, ti)
+
+
 %%It will print the magnitude of the input signal 
 	figure
-	plot(x_s, s, 'Linewidth',4)
-    axis([0 (length(s) - 1) -inf inf ])
-	xlabel('$v_x$','Interpreter','latex')
+    pause(0.00001);
+    frame_h = get(handle(gcf),'JavaFrame');
+    set(frame_h,'Maximized',1); 
+	plot(x_s, s1,'k-.' ,'Linewidth',4)
+    if s2 ~= 0
+        hold
+        plot(x_s, s2,'k' ,'Linewidth',4)
+    end
+    ymin = floor(min( [min(s1) min(s2)]));
+    ymax = ceil(max([max(s1) max(s2)] ) );
+    axis([0 (length(s1) - 1) ymin ymax ])
+	xlabel('$v$ (m/s)','Interpreter','latex')
     ylabel(y_la,'Interpreter','latex')
-	set(gca,'FontSize',26,'Fontname','CMU Serif Roman')
+    set(gca,'linewidth',2.5)
+	set(gca,'FontSize',42,'Fontname','CMU Serif Roman')
 	pbaspect([4 3 1])
     saveas(gcf, [ti '.jpg'])
-end
-
+    
 end
--- run_sar.m
+++ run_sar.m
@@ -7,29 +7,21 @@ for kk = 1 : over
   [r_PLSR(kk), a_PLSR(kk), rsandb(kk), asandb(kk), t_v_x(kk), t_v_y(kk)] =  SAR((kk - 1) * step, 0);
 end
 
-my_plot(t_v_x - x,  x, '$\tilde{v}_x - v_x$','1-1')
-my_plot(r_PLSR, x, 'Range PLSR','1-3')
-my_plot(a_PLSR, x, 'Azimuth PLSR','1-4')
-my_plot(rsandb, x, 'Range pulse width','1-5')
-my_plot(asandb, x, 'Azimuth pulse width','1-6')
+my_plot(t_v_x - x, 0, x, '$\tilde{v}_x - v_x$ (m)','errx_vyfix')
+my_plot(r_PLSR, a_PLSR, x, 'PLSR (dB)','plsr_vyfix')
+my_plot(rsandb, asandb, x, 'Pulse width (m)','sand_vyfix')
 
 %% v_y change, v_x = 0
 for kk = 1 : over
    [r_PLSR(kk), a_PLSR(kk), rsandb(kk), asandb(kk), t_v_x(kk), t_v_y(kk)] = SAR(0, (kk-1) * step);
 end
-my_plot(real(t_v_y) - x,  x, '$\tilde{v}_y - v_y$','2-2')
-my_plot(r_PLSR, x, 'Range PLSR','2-3')
-my_plot(a_PLSR, x, 'Azimuth PLSR','2-4')
-my_plot(rsandb, x, 'Range pulse width','2-5')
-my_plot(asandb, x, 'Azimuth pulse width','2-6')
+my_plot(real(t_v_y) - x, 0, x, '$\tilde{v}_y - v_y$ (m/s)','erry_vxfix')
+my_plot(r_PLSR, a_PLSR, x, 'PLSR (dB)','plsr_vxfix')
+my_plot(rsandb, asandb, x, 'Pulse width (m)','sand_vxfix')
 %% v_y = v_x 
 for kk = 1 : over
    [r_PLSR(kk), a_PLSR(kk), rsandb(kk), asandb(kk), t_v_x(kk), t_v_y(kk)] = SAR((kk-1) * step, (kk-1) * step);
 end
-my_plot(t_v_x - x,  x, '$\tilde{v}_x - v_x$','3-1')
-my_plot(real(t_v_y) - x,  x, '$\tilde{v}_y - v_y$','3-2')
-my_plot(r_PLSR, x, 'Range PLSR','3-3')
-my_plot(a_PLSR, x, 'Azimuth PLSR','3-4')
-my_plot(rsandb, x, 'Range pulse width','3-5')
-my_plot(asandb, x, 'Azimuth pulse width','3-6')
-close all
+my_plot(t_v_x - x, real(t_v_y) - x, x, '$\tilde{v} - v$ (m/s)','errx_')
+my_plot(r_PLSR, a_PLSR, x, 'PLSR (dB)','plsr_')
+my_plot(rsandb, asandb, x, 'Pulse width (m)','sand_')
