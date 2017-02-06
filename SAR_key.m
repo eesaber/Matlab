1649f360d13cfa1072f11d71c21712fce8e184d6
nogiza <fbookzone@gmail.com>
2017-02-06 13:25:30 +0800

    Ambiguity function left

--- SAR_key.m
+++ SAR_key.m
@@ -37,62 +37,36 @@ function [] = SAR_key(vx, vy)
     %purinto(s_rc); % Print the s_r
     
     %% Key Stone transform - RCMC for range curveture 
-    %Fs_rc = fftshift(Fs_rc, 2) ; % Not sure if "fftshift" is needed ????!!!!!!
     % Interpolation 
-    f_tau = 0 : 2 * B / 2^tau_nt2 : 2*B -1 ;
+    f_tau = linspace(0, 4*B -1, 2^tau_nt2 ); 
+    shift = 0; % FFTshift false
+    if shift == 1
+    	f_tau = linspace(-2*B, 2*B -1, 2^tau_nt2 ); 
+    	Fs_rc = fftshift(Fs_rc, 2);
+    end
     for i = 1 : 2^tau_nt2
     	t(:, i) = eta * sqrt(f_0 / (f_0 + f_tau(i)) ).' ;    
     end
-    
     for m = 1 : 2^tau_nt2
     	Fs_1(:, m) = interp1(eta, Fs_rc(:,m).', t(:,m).', 'spline', 0).';
-    	%{
-    	for n = 1 : length(eta)
-    		if t(1) < eta(n) && t(length(eta)) > eta(n)
-    			[~, idx] = min(abs(eta(n) - t(:,m));
-    			idx_u = idx + (eta(n) > t(:,idx)) ; 
-    			idx_l = idx_u - 1;
-    			S_1(n,m) = ((eta(n) - t(:,idx_l)) * S_rc(idx_l, m) + (t(:,idx_u) - eta(n)) * S_rc(idx_l, m))/ (t(:,idx_u) - t(:,idx_l)) ;
-    		elseif t(1) > eta(n) 
-    			continue
-    		else
-    			break
-    		end
-    	end
-    	%}
     end
-    s_1 = ifft(ifftshift(Fs_1, 2).').'; % If the central of f_tau is 0, then ifftshift is needed. If not so, do not do ifftshift 
-    
- 
-    %% RCMC for range walk 
-    v_rt = 25 ; % 修正?
-    Fh_rw = exp(j * 4 * pi / c * t / 2 * v_rt .* repmat(f_tau, length(eta), 1) );
-    Fs_2 = Fs_1 .* Fh_rw ; 
-    s_2 = ifft(Fs_2.').';
-    
+    if shift == 1
+    	s_1 = ifft(ifftshift(Fs_1, 2).').'; % If the central of f_tau is 0, then ifftshift is needed. If not so, do not do ifftshift 
+    else
+    	s_1 = ifft(Fs_1.').';
+    end
     %purinto(s_1);
-    purinto(s_2);
+    %% RCMC for range walk 
+    v_rt = 10 * x_n / R_0; 
+    Fh_rw = exp(j * 4 * pi / c * t / 2 * v_rt .* repmat(f_tau, length(eta), 1) );    
+    Fs_2 = Fs_1 .* Fh_rw ; 
+    if shift == 1
+        s_2 = ifft(ifftshift(Fs_2, 2).').';
+    else
+        s_2 = ifft(Fs_2.').';
+    end
+    purinto(s_2)
     %% Ambiguity function
-    H_AF(s_2(:, 1024));
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    %H_AF(s_2(:, 1024));
     
 end
