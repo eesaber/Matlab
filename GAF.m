023ddefc392bb9e379d0ad0a706d71be84adda34
EE527 <r05942013@ntu.edu.tw>
2017-03-15 17:49:43 +0800

    temp

--- GAF.m
+++ GAF.m
@@ -4,9 +4,9 @@ function [AF] = GAF(s, total_ord, n)
 % the order, at begin, of the polynomial phase signal. "n" is the order
 % "NOW". Return an n-th order ambiguity function.
 	if mod(length(s), total_ord * 2)
-		s = [s zeros(1, total_ord * 2 - mod(length(s), total_ord * 2))];
+		%s = [s zeros(1, total_ord * 2 - mod(length(s), total_ord * 2))];
 	end
-	t_mov = length(s) / total_ord / 2;
+	t_mov = int16(length(s) / total_ord / 2);
 	for qq = 1 : n - 1
 		s = [zeros(1, t_mov) s(1: length(s) - t_mov) ] .* conj( [ s(t_mov + 1:end) zeros(1, t_mov)] ) ;
 	end
