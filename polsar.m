a10a38fc531d4334e31b6c99a2b2a6d8a3f42b9b
EE527 <r05942013@ntu.edu.tw>
2017-09-08 17:36:03 +0800

    4-comp+eigen algo debuging and optimizing.

--- polsar.m
+++ polsar.m
@@ -1,5 +1,3 @@
-clc
-%{
 % Data size: 8735*23499, no header
 % Data IO
 clear,clc
@@ -105,11 +103,11 @@ if(0)	% Plot the Pauli-decomposition.
 		plot_para('Maximize',true)
 	%}
 end
-%}																																																													
+																																																									
 %% Four-component decomposition (option: compensate the orientation)
 fprintf('Compensate the oriented angle or not? CAUTION! THIS WILL CHANGE hh_hh, vv_vv, etc PERMANENTLY. \n')
 P_t = hh_hh + vv_vv + 2*hv_hv;
-%{
+
 if(0)
 	fprintf('Yes.  \n')
 	filename = 'Four_compoR';
@@ -249,7 +247,7 @@ if(0)	% Plot the 4-component decomposition.
 	FourCompo(:,:,1) = 10*log10(P_d);
 	FourCompo(:,:,2) = 10*log10(f_v);
 	FourCompo(:,:,3) = 10*log10(P_s);
-	clear P_d  f_v P_s f_c f_d f_s 
+	
 	FourCompo(FourCompo < low_) = low_;
 	FourCompo(FourCompo > up_) = up_;
 	FourCompo = (FourCompo-low_)/(up_-low_);
@@ -263,7 +261,7 @@ if(0)	% Plot the 4-component decomposition.
 		%plot_para('Maximize',true)
 	clear FourCompo filename
 end
-%}
+clear P_d  f_v P_s f_c f_d f_s 
 %% eigenvalue model-based 4-component decomposition
 % Build table
 if(0) 
@@ -286,7 +284,7 @@ if(0)
 
 	C_v(:,:,1) = 1/8*[3, 0, 1; 0, 2, 0; 1, 0, 3];
 	C_v(:,:,2) = 1/15*[8, 0, 2; 0, 4, 0; 2, 0, 3];
-	C_v(:,:,3) = 1/8*[3, 0, 2; 0, 4, 0; 2, 0, 8];
+	C_v(:,:,3) = 1/15*[3, 0, 2; 0, 4, 0; 2, 0, 8];
 	table = zeros(138600,9); 
 	qq = 1;
 	for k = 1 : numel(theta)
@@ -302,7 +300,6 @@ if(0)
 				for beta_it = 1: numel(beta)
 					for chi_it = 1: numel(chi)
 						for cv_it = 1 : 3
-							% template = [x^2, 0, x; 0,0,0; x^*,0,1];
 							C_s = 1/(abs(chi(chi_it))^2 + 1)*[abs(chi(chi_it))^2, 0, chi(chi_it); 0,0,0; conj(chi(chi_it)),0,1];
 							C_d = 1/(abs(beta(beta_it)^2) + 1)*[beta(beta_it)^2, 0, beta(beta_it); 0,0,0; conj(beta(beta_it)),0,1];
 							C = f_s(n)*C_s + f_d(m)*R*C_d*R.' + f_v*C_v(:,:,cv_it);
@@ -335,84 +332,62 @@ fprintf('\n')
 %% Query	 
 P_h = single(zeros(row, col));
 P_s = P_h; P_d = P_h; P_v = P_h;
-
 [t_row,~,~] = size(table);
 
-% 2780 : 2880
-% 8300 : 8500		%xlim([8100 8900])
-		%ylim([2800 3100])
-% 9 elements in entry: f_s(n), f_d(m), f_v, p(3), p(2), p(1), alpha3, alpha2, alpha1
 D_1 = table(:,:,1);
 D_2 = table(:,:,2);
 D_3 = table(:,:,3);
 D_4 = table(:,:,4);
 co_1 = repmat([zeros(1,3), 0.5, 0.5, zeros(1,4)], t_row, 1); 
-left = [5800 6100];
-down = [14100 14700];
-parfor m = 5800 : 6100
+left = [2800 3100];
+down = [8100 8900];
+parfor m = left(1) : left(2)
 	for n = 14100: 14700
-	C = [hh_hh(m,n), sqrt(2)*hh_hv(m,n), hh_vv(m,n); sqrt(2)*conj(hh_hv(m,n)), 2*hv_hv(m,n), sqrt(2)*hv_vv(m,n);......
-		conj(hh_vv(m,n)), sqrt(2)*conj(hv_vv(m,n)), vv_vv(m,n)];
-	[V, p] = eig(C);
-	alpha = acos(abs(V(1,:) + V(3,:))/sqrt(2));
-	delta = acos(abs((V(3,:)-V(1,:)+1j*sqrt(2)*V(2,:))/2)./sin(alpha));
-	hel = sin(alpha.^2).*(cos(delta.^2)- sin(delta.^2));
-	% f_s(n), f_d(m), f_v, p(3:-1:2).', alpha.'
-	P_h(m,n) = abs(sum(hel*p));
-	p = diag(p)/sum(diag(p));
-	P_r = P_t(m,n) - P_h(m,n);
+		C = [hh_hh(m,n), sqrt(2)*hh_hv(m,n), hh_vv(m,n); sqrt(2)*conj(hh_hv(m,n)), 2*hv_hv(m,n), sqrt(2)*hv_vv(m,n);......
+			conj(hh_vv(m,n)), sqrt(2)*conj(hv_vv(m,n)), vv_vv(m,n)];
+		[V, p] = eig(C);
+		alpha = acos(abs(V(1,:) + V(3,:))/sqrt(2));
+		delta = acos(abs((V(3,:)-V(1,:)+1j*sqrt(2)*V(2,:))/2)./sin(alpha));
+		hel = sin(alpha.^2).*(cos(delta.^2)- sin(delta.^2));
+		% f_s(n), f_d(m), f_v, p(3:-1:2).', alpha.'
+		P_h(m,n) = abs(sum(hel*p));
+		p = diag(p)/sum(diag(p));
+		P_r = P_t(m,n) - P_h(m,n);
 
-	eta = 0.25*atan((-2*sqrt(2)*real(V(3,:)-V(1,:).*conj(V(2,:))))/abs(V(3,:)-V(1,:)).^2 - abs(V(2,:)).^2);
-	eta(eta>pi/4) = eta(eta>pi/4) - pi/2;
-	d_theta = abs(eta(3)-eta(2));
+		eta = 0.25*atan((-2*sqrt(2)*real(V(3,:)-V(1,:).*conj(V(2,:))))/abs(V(3,:)-V(1,:)).^2 - abs(V(2,:)).^2)+pi/4;
+		eta(eta>pi/4) = eta(eta>pi/4) - pi/2;
+		d_theta = abs(eta(3)-eta(2));
 
-	if d_theta > pi/4
-		d_theta = -d_theta + pi/2;
+		if d_theta > pi/4
+			d_theta = -d_theta + pi/2;
+		end
+		if d_theta <= pi/24
+			D = D_1;
+		elseif pi/24 < d_theta <= pi/8
+			D = D_2;
+		elseif pi/8 < d_theta <= 5*pi/24
+			D = D_3;
+		elseif 5*pi/24 < d_theta <= pi/4
+			D = D_4;
+		end
+		S = sum(0.5*(ones(t_row,1)*p(2:3).' - D(:,5:6)).^2, 2);
+		[~, ind_] = min(S + sum(D(:,4:6).*((ones(t_row,1)*alpha - D(:,7:9))*2/pi).^2, 2));
+		if P_r > 0
+			P_s(m,n) = P_r*D(ind_, 1); 
+			P_d(m,n) = P_r*D(ind_, 2); 
+			P_v(m,n) = P_r*D(ind_, 3);
+		end
 	end
-	if d_theta <= pi/24
-		u = 1;
-		D = D_1;
-	elseif pi/24 < d_theta <= pi/8
-		u = 2;
-		D = D_2;
-	elseif pi/8 < d_theta <= 5*pi/24
-		u = 3;
-		D = D_3;
-	elseif 5*pi/24 < d_theta <= pi/4
-		u = 4;
-		D = D_4;
-	end
-	S = sum(0.5*(ones(t_row,1)*p(3:-1:2).' - D(:,4:5)).^2, 2);
-	[~, ind_] = min(S + sum(D(:,4:6).*((ones(t_row,1)*alpha - D(:,7:9))*2/pi).^2, 2));
-	P_d(m,n) = P_r*D(ind_, 2); 
-	P_s(m,n) = P_r*D(ind_, 1); 
-	P_v(m,n) = P_r*D(ind_, 3);
-	%{
-	%para_ = repmat([zeros(3,1), p(3), p(2), zeros(4,1)], t_row, 1);
-	%S = ((para_ - table(:,:,u)).*co_).^2 ;
-	%para_ =  ones(t_row,1)*[zeros(5), alpha.']*2/pi;
-	%co_ = [zeros(t_row, 5), table(: ,4:5, u), 1-table(:, 4:5, u)];
-	%[~,ind_] = min(sum( S + ((table(:,:,u)-para_).*co_).^2, 2));
-	P_d(m,n) = P_r*table(ind_, 2, u); 
-	P_s(m,n) = P_r*table(ind_, 1, u); 
-	P_v(m,n) = P_r*table(ind_, 3, u);
-	%}
-	end
-	%{
-	if ~mod(m,20)
-		fprintf('.') 
-	end
-	%}
 end
 clear table D_1 D_2 D_3 D_4
-%%
-if(1)	% Plot the 4-component + eigenvalue decomposition.
+%% Plot the 4-component + eigenvalue decomposition.
+if(0)	
 	FourCompo = single(zeros(row, col, 3));
 	up_ = 20; low_ = -30;
-	FourCompo(:,:,1) = 10*log10(P_d);
+	FourCompo(:,:,1) = 10*log10(P_s);
 	FourCompo(:,:,2) = 10*log10(P_v);
-	FourCompo(:,:,3) = 10*log10(P_s);
-	clear P_d  P_v P_s P_v P_h
+	FourCompo(:,:,3) = 10*log10(P_d);
+	%clear P_d  P_v P_s P_v P_h
 	FourCompo(FourCompo < low_) = low_;
 	FourCompo(FourCompo > up_) = up_;
 	FourCompo = (FourCompo-low_)/(up_-low_);
@@ -424,5 +399,31 @@ if(1)	% Plot the 4-component + eigenvalue decomposition.
 		ylim(left)
 		plot_para('Filename','4com_eigen','Maximize',true,'Ratio', [7 5 1])
 		%plot_para('Maximize',true)
-	%clear FourCompo
+	clear FourCompo
+end
+if(0)
+	figure(14)
+		imagesc(10*log10(P_s))
+		set(gca,'Ydir','normal')
+		title('$10 log P_{s}2$', 'Interpreter', 'latex'), colorbar, colormap gray;
+		xlim(down)
+		ylim(left)
+	figure(15)
+		imagesc(10*log10(P_d))
+		set(gca,'Ydir','normal')
+		title('$10 log P_{d}$', 'Interpreter', 'latex'), colorbar, colormap gray;
+		xlim(down)
+		ylim(left)
+	figure(16)
+		imagesc(10*log10(abs(P_v)))
+		set(gca,'Ydir','normal')
+		title('$10 log P_{v}$', 'Interpreter', 'latex'), colorbar, colormap gray;
+		xlim(down)
+		ylim(left)
+	figure(17)
+		imagesc(10*log10(P_h))
+		set(gca,'Ydir','normal')
+		title('$10 log P_{h}$', 'Interpreter', 'latex'), colorbar, colormap gray;
+		xlim(down)
+		ylim(left)
 end
