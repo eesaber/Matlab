9907dd10e7621986d240764f43f21b371806fdbb
Nogiza <fbookzone@gmail.com>
2017-12-24 14:23:22 +0800

    Writing NMF

--- PolSAR/src/NMF_polsar.m
+++ PolSAR/src/NMF_polsar.m
@@ -1,15 +1,42 @@
 %% The code implement decomposition by using non-negative matrix factorization
 clear 
 clc
-%% Simulated data
-% declaration of variable
+test = 1;
 q = 9; 
 M = 100;
 k = 8;
-N_az = 100; N_ra = 100; % az==row, ran == col
-N = N_az*N_ra; % Image with size (100x100) 
-%C = random(a,M); % Coherent target space (qxM)
-%A = random(M,k); % Assembly matrix (Mxk)
-X = gen_map(k, N_az, N_ra,); % Spatial distribution matrix (kxN)
+if test 
+	%% Simulated data
+	% declaration of variable
+	N_az = 100; N_ra = 100; % az==row, ran == col
+	N = N_az*N_ra; % Image with size (100x100) 
+	%C = random(a,M); % Coherent target space (qxM)
+	%A = random(M,k); % Assembly matrix (Mxk)
+	X = gen_map(N_az, N_ra, k); % Spatial distribution matrix (kxN)
+	D = 1;
+	Y = D*X;
+else
+	%% Real data import
+	[hh_hh, hv_hv, vv_vv, hh_hv, hh_vv, hv_vv] = data_io();
+	n = numel(hh_hh);
+	Y = [reshape(hh_hh, [1, n]); reshape(hv_hv, [1, n]); reshape(vv_vv, [1, n]); ......
+		real(reshape(hh_hv, [1, n]))/sqrt(2); imag(reshape(hh_hv, [1, n]))/sqrt(2);
+		real(reshape(hh_vv, [1, n]))/sqrt(2); imag(reshape(hh_vv, [1, n]))/sqrt(2);
+		real(reshape(hv_vv, [1, n]))/sqrt(2); imag(reshape(hv_hv, [1, n]))/sqrt(2)];
+	[N_az, N_ra] = Y.size;
+	clear  hh_hh hv_hv vv_vv hh_hv hh_vv hv_vv
+end
 
-
+%% Non-negative matrix factorization
+[A_sol, X_sol] = nnmf(Y, k, 'Display', 'iter', 'UseParallel', true);
+X_sol = reshape(X_sol, [N_az, N_ra, k]);
+figure(1)
+	for rr = 1 : k
+		subplot(2, k/2, rr)
+		imagesc(~map(:, :, rr))
+        title(['# of ' num2str(rr)])
+		set(gca, 'YDir','normal')
+		axis off
+        colormap gray
+	end
+	axis on
--- PolSAR/src/gen_map.m
+++ PolSAR/src/gen_map.m
@@ -1,31 +1,54 @@
-function [map] = gen_map(n_map, col_size, row_size)
-	mask = zeros(row_size, col_size, 1);
+function [map] = gen_map(varargin)
+% GEN_MAP is used to generate a spatial distribution of scatterers for verification.
+% The return value of gen_map is an 3D matrix of size [channel, row, column].
+% How to call? Variable are in order of (row size, coloumn size, number of channel).
+
+    switch nargin 
+    	case 0
+	        n_map = 8; 
+	        col_size = 100; 
+	        row_size = 100;
+	    case 3
+	    	optargs = {eps 17 @magic};
+        	optargs(1:nargin) = varargin;
+        	[row_size, col_size, n_map] = optargs{:};
+        otherwise
+        	disp('NUMBER OF INPUT VARIABLE ERROR')
+    end
+
 	map = zeros(row_size, col_size, n_map);
-	rng(1024);
-	temp = [ones(col_size*row_size*0.3) zeros(col_size*row_size*0.7)];
-	% 1. Random asymmetric.
-	map(:, :, 1) = map(:, :, 1) + reshape(temp(randperm(numel(temp))), [row_size, col_size, 1]); 
-	% 2. First kind of Building.
-
-	% 3. Second kind of Building.
-
-	% 4. Road 
-
-	% 5. First kind of tree
-
-	% 6. Second kind of tree
-
-	% 7. 
-
+	rng(1); % control random seed
+	
+	% 1. First kind of Building.  
+    map(1:row_size/2, 1:col_size/2, 2) = reshape(rand(row_size/2), [row_size/2, col_size/2, 1])>0.3;
+	% 2. Second kind of Building.
+    map(row_size/2+1:end, 1:col_size/2, 3) = reshape(rand(row_size/2), [row_size/2, col_size/2, 1])>0.3;
+	% 3. Road 
+    map(:, 1:5:col_size/2, 4) = 1;
+    map(:, :, 4) = map(:, :, 4).*reshape(rand(row_size), [row_size, col_size, 1])>0.3;
+	% 4. First kind of tree
+	cen = [row_size/2 3*col_size/4];
+	[x_, y_] = meshgrid(1:row_size, 1:col_size);
+    map(:, :, 5) = (x_-cen(2)).^2+(y_-cen(1)).^2 < min(row_size/4, col_size/4).^2;
+    map(:, :, 5) = map(:, :, 5).*reshape(rand(row_size), [row_size, col_size, 1])>0.3;
+	% 5. Second kind of tree
+	map(:, col_size/2+1:end, 6) = 1;
+	map(:, :, 6) = map(:, :, 6).*reshape(rand(row_size), [row_size, col_size, 1])>0.3;
+	% 6. Whatever
+	map(:, :, 7) = (row_size/col_size*x_ + y_ <= row_size).*rand(row_size)>0.3;
+	% 7. Whatever2
+	map(:, :, 1) = (row_size/col_size*x_ + y_ >= row_size).*rand(row_size)>0.3;
+	% 8. Random asymmetric	
+	map(:,:, 8) = reshape(sum(map,3)<1, [row_size, col_size, 1]);
 	% Plot the sptial distribution 
-	for rr = 1 : k
-    figure(k)
-        imagesc(map(:, :, rr))
-        %xlabel('range')
-        %ylabel('azimuth')
-        title(['# of ' num2str(k)])
+	figure(957)
+	for rr = 1 : n_map
+		subplot(2, n_map/2, rr)
+		imagesc(~map(:, :, rr))
+        title(['# of ' num2str(rr)])
+		set(gca, 'YDir','normal')
+		axis off
+        colormap gray
 	end
-	pause
-	close all
-
+	axis on
 end
