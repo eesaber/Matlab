ff92e60827a806441831bb605f30c9367addec12
Nogiza <fbookzone@gmail.com>
2017-12-27 13:43:44 +0800

    Rewrite Vis_Co.m and able to plot asymmetric target.

--- PolSAR/src/Vis_Co.m
+++ PolSAR/src/Vis_Co.m
@@ -3,7 +3,7 @@ function Vis_Co(k_p)
 %	Vis_Co(k_p) is a function to visualize the scattering mechanism.
 
     % Generate hemisphere.
-	SP_NUM = 30;
+	SP_NUM = 300;
 	thetavec = linspace(0,pi/2,SP_NUM/2);
 	phivec = linspace(0,2*pi,2*SP_NUM);
 	[th, ph] = meshgrid(thetavec,phivec);
@@ -18,7 +18,8 @@ function Vis_Co(k_p)
         fprintf('Vis_Co IS IN TESTING MODE!\n')
         psi = 0*(pi/180);
         R = [cos(psi), sin(psi); -sin(psi), cos(psi)]; % Rotate counter-clockwisely with psi.
-        S = R.'*[1, 0; 0, 0]*R;
+        S = R.'*[1, -1j; -1j, -1]*R;
+		%S = R.'*[1, 0; 0, 0]*R;
 		if isreal(S)
 			fprintf('Scattering matrix is:\n %f %f\n %f %f \n',S)
 		else
@@ -26,16 +27,14 @@ function Vis_Co(k_p)
 		end
         k_p = 1/sqrt(2)*[S(1,1)+S(2,2), S(1,1)-S(2,2), 2*S(1,2)].';    
 	end
-	
-	k_p = k_p/sqrt(sum(abs(k_p).^2));
     % Approach 1. Brute Force 
     n = 180;
-    psi = linspace(0,pi/2,n);
+    psi = linspace(0,pi,n);
     c = reshape(cos(2*psi),[1,1,n]);
     s = reshape(sin(2*psi),[1,1,n]);
     R = reshape([ones(1,1,n), zeros(1,1,n), zeros(1,1,n); zeros(1,1,n), c, -s; zeros(1,1,n), s, c],[3,3*n]).';
     temp = R*k_p;
-    [~, ind] = min(abs(temp(3:3:end)));
+    [sup_, ind] = min(abs(temp(3:3:end)));
     psi_br = psi(ind);
     % Approach 2. Analytical Solution
     alpha = acos(abs(k_p(1)));
@@ -46,39 +45,49 @@ function Vis_Co(k_p)
 	end
     phi_2 = angle(k_p(2));
     phi_3 = angle(k_p(3));
-    psi = mod((2*(cos(phi_2-phi_3)>=0)-1)*(2*beta - mod(2*beta,pi) ......
+    psi_ana = mod((2*(cos(phi_2-phi_3)>=0)-1)*(2*beta - mod(2*beta,pi) ......
     	+ mod(atan(tan(2*beta)*abs(cos(phi_2-phi_3))), pi))/4, pi/2);
-    fprintf('Brute force: %f, Analytical: %f \n', psi_br/pi*180, psi/pi*180)
-    %psi = psi_br;
-    % Generate the pattern 
-    R = [1, 0, 0; 0, cos(2*psi), -sin(2*psi); 0, sin(2*psi), cos(2*psi)];
-    k_p = abs((R*k_p)).*[1, cos(2*psi), sin(2*psi)].';  
-    mu = [k_p(2) k_p(3)]; % Let k_p(2) and k_p(3) be x-axis and y-axis respectively.
-	sig = [0.001, 0; 0, 0.001];
-	F = mvnpdf([x(:) y(:)],mu, sig);
-	F = reshape(F,size(th));
+    fprintf('Brute force: %f, Analytical: %f \n', psi_br/pi*180, psi_ana/pi*180)    
+	
+	% Generate the pattern 
+	% Check if there exist more than one min value
+	F = zeros(size(th));
+	[x_plain, y_plain] = meshgrid(linspace(-1,1,SP_NUM),linspace(-1,1,SP_NUM));
+	F_plain = zeros(size(x_plain));
+	allkp_3 = temp(3:3:end);
+	psi_p = psi(abs(abs(allkp_3) - sup_) < 2.2251e-10);
+	for n = 1 : numel(psi_p)
+		R = [1, 0, 0; 0, cos(2*psi_p(n)), -sin(2*psi_p(n)); 0, sin(2*psi_p(n)), cos(2*psi_p(n))];
+		k_pp = abs((R*k_p)).*[1, cos(2*psi_p(n)), sin(2*psi_p(n))].';  
+		k_pp = k_pp/sqrt(sum(abs(k_pp).^2));
+		mu = [k_pp(2) k_pp(3)]; % Let k_p(2) and k_p(3) be x-axis and y-axis respectively.
+		sig = [0.0005, 0; 0, 0.0005];
+
+		temp_F = mvnpdf([x(:) y(:)],mu, sig);
+		F = F + reshape(temp_F,size(th));
+		temp_P = mvnpdf([x_plain(:) y_plain(:)],mu, sig);
+		F_plain = F_plain + reshape(temp_P,size(x_plain));
+	end
 	F = ind2rgb(uint8((F/max(max(F)))*255),jet);
-    
-    [x_plain, y_plain] = meshgrid(linspace(-1,1,SP_NUM),linspace(-1,1,SP_NUM));
-    F_plain = mvnpdf([x_plain(:) y_plain(:)],mu, sig);
-    F_plain = reshape(F_plain,size(x_plain));
-    
-    close all
-    cd /home/akb/Code/Matlab
+	F_plain = F_plain/max(max(F_plain));
+    %close all
+	if isunix
+		cd /home/akb/Code/Matlab
+	end
     
 	figure
-	%surf(x,y,z,F,'EdgeColor','none')
-	surf(x,y,z,'FaceColor','w')
+	surf(x,y,z,F,'EdgeColor','none')
+	%surf(x,y,z,'FaceColor','w')
 	view(135,30);
-	xlabel('$|k''_{p2}| \cos (2 \psi_m)$','Interpreter', 'latex')
-	ylabel('$|k''_{p3}| \sin (2 \psi_m)$','Interpreter', 'latex')
-	zlabel('$|k''_{p1}|$','Interpreter', 'latex')
+	xlabel('$|k''_{p2}| \cos (2 \psi_m) / \|\bar{k}_p\|_2$','Interpreter', 'latex')
+	ylabel('$|k''_{p3}| \sin (2 \psi_m)/ \|\bar{k}_p\|_2 $','Interpreter', 'latex')
+	zlabel('$|k''_{p1}| / \|\bar{k}_p\|_2 $','Interpreter', 'latex')
 	plot_para('Ratio',[2 2 1],'Maximize', true,'Filename', 'VisSph')
     figure
     imagesc(x_plain(1,:), y_plain(:,1),-F_plain/max(max(F_plain)))
-    xlabel('$|k''_{p2}| \cos (2 \psi_m)$','Interpreter', 'latex')
-	ylabel('$|k''_{p3}| \sin (2 \psi_m)$','Interpreter', 'latex')
+    xlabel('$|k''_{p2}| \cos (2 \psi_m) / \|\bar{k}_p\|_2$','Interpreter', 'latex')
+	ylabel('$|k''_{p3}| \sin (2 \psi_m) / \|\bar{k}_p\|_2$','Interpreter', 'latex')
     set(gca,'Ydir','normal','XGrid','on','YGrid','on')
     colormap gray
-    plot_para('Ratio',[4 3 1],'Maximize', true,'Filename', 'VisSph_Dipo')
+    plot_para('Ratio',[4 3 1],'Maximize', true)%'Filename', 'VisSph_Dipo')
 end
