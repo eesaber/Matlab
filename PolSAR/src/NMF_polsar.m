e8fa08f759d38e8fc19ecd0baad84552972c4798
Nogiza <fbookzone@gmail.com>
2018-02-05 13:05:22 +0800

    Vis_Co: remove testing mode, support multiple k_p. NMF_polsar: complete of generate C.

--- PolSAR/src/NMF_polsar.m
+++ PolSAR/src/NMF_polsar.m
@@ -4,16 +4,15 @@ clc
 test = 1;
 q = 9; 
 M = 100;
-k = 8;
+Q = 8;
 %% Read Data
 if test 
 	% Simulated data
 	% declaration of variable
 	N_az = 100; N_ra = 100; % az==row, ran == col
 	N = N_az*N_ra; % Image with size (100x100) 
-	%C = random(a,M); % Coherent target space (qxM)
-	%A = random(M,k); % Assembly matrix (Mxk)
-	X = gen_map(N_az, N_ra, k); % Spatial distribution matrix (kxN)
+	%A = random(M,Q); % Assembly matrix (MxQ)
+	X = gen_map(N_az, N_ra, Q); % Spatial distribution matrix (QxN)
 	D = 1;
 	Y = D*X;
 else
@@ -26,6 +25,27 @@ else
 		real(reshape(hv_vv, [1, n]))/sqrt(2); imag(reshape(hv_hv, [1, n]))/sqrt(2)];
 	[N_az, N_ra] = Y.size;
 	clear  hh_hh hv_hv vv_vv hh_hv hh_vv hv_vv
+end
+%% Build coherency target space 
+%{
+rho = linspace(0,1,101);
+theta = linspace(0,2*pi-0.001,360);
+[kp_2, kp_3] = pol2cart(meshgrid(rho, theta));
+kp_2 = reshape(1, numel(kp_2));
+kp_3 = reshape(1, numel(kp_3));
+kp_1 = sqrt(1 - kp_2.^2 - kp_3.^2);
+%} 
+mu = 0;
+sigma = 1;
+size_K = 100;
+k_p = normrnd(mu,sigma,[3, size_K]) + 1j*normrnd(mu,sigma,[3, size_K]);
+C = zeros(9, size_K);
+for r = 1 : size_K
+	%k_p(:,r) = exp(-1j*angle(k_p(1,r)))*k_p(:,r);
+	k_p(:,r) = k_p(:,r)/norm(k_p(:,r),2);
+	t = k_p(:,r)*k_p(:,r)';
+	C(:,r) = [real(t(1,1)); real(t(2,2)); real(t(3,3)); sqrt(2)*real(t(1,2)); sqrt(2)*imag(t(1,2)); ...
+			sqrt(2)*real(t(1,3)); sqrt(2)*imag(t(1,3)); sqrt(2)*real(t(2,3)); sqrt(2)*imag(t(2,3))];
 end
 
 %% Non-negative matrix factorization
