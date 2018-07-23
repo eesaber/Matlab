8811e4c6cc55cf1a08d02e6fed12067d6eac8f99
AKB <r05942013@ntu.edu.tw>
2018-07-23 17:32:10 +0800

    Fix bug in eigen-decomposition.m

--- PolSAR/src/Eigen_decomp.m
+++ PolSAR/src/Eigen_decomp.m
@@ -23,30 +23,32 @@ function [H, lambda] = Eigen_decomp(varargin)
         T = parse_.Results.T;
         H = zeros(1, num); A_1 = zeros(1, num); A_2 = zeros(1, num); lambda = zeros(3,num);
         alpha_bar = zeros(1, num);
-        
-        parfor r = 1 : num
-            %if sum(sum(parse_.Results.T(:,:,r)== zeros(3,3))) == 9
-            %    continue;
-            %end
-            [U, L] = eig(T(:,:,r));
-            P = diag(L)/sum(diag(L));
-            H(r) = -sum(P.*log(P)/log(3));
-            A_temp = sort(diag(L),'descend');
-            A_1(r) = (A_temp(2)-A_temp(3))/(A_temp(2)+A_temp(3));
-            A_2(r) = (A_temp(1)-A_temp(2))/(A_temp(1)+A_temp(2));
-            lambda(:,r) = A_temp;
-            alpha_bar(r) = sum(P'.*acosd(abs(U(1,:))));
+        seg = [1, num/4, num/2, 3*num/4, num];
+        for b = 1 : 4
+            parfor r = ceil(seg(b)) : floor(seg(b+1))
+                if sum(sum(T(:,:,r)== zeros(3,3))) == 9
+                    continue;
+                end
+                [U, L] = eig(T(:,:,r));
+                P = diag(L)/sum(diag(L));
+                H(r) = -sum(P.*log(P)/log(3));
+                A_temp = sort(diag(L),'descend');
+                A_1(r) = (A_temp(2)-A_temp(3))/(A_temp(2)+A_temp(3));
+                A_2(r) = (A_temp(1)-A_temp(2))/(A_temp(1)+A_temp(2));
+                lambda(:,r) = A_temp;
+                alpha_bar(r) = sum(P'.*acosd(abs(U(1,:))));
+            end
         end
-        e = cputime -t;
-        disp(['Execution time:' num2str(e)])
-        %%
-        H = single(real(reshape(H, im_size)));
-        A_1 = single(reshape(A_1, im_size));
-        A_2 = single(reshape(A_2, im_size));
-        alpha_bar = single(reshape(alpha_bar, im_size));
-        lambda = single(reshape(lambda, [im_size 3]));
-        disp('Saving results....')
-        save([dir parse_.Results.Filename '.mat'],'-v7.3', 'H', 'A_1', 'A_2', 'lambda','alpha_bar');
+            e = cputime -t;
+            disp(['Execution time:' num2str(e)])
+            %%
+            H = single(real(reshape(H, im_size)));
+            A_1 = single(reshape(A_1, im_size));
+            A_2 = single(reshape(A_2, im_size));
+            alpha_bar = single(reshape(alpha_bar, im_size));
+            lambda = single(reshape(lambda.', [im_size 3]));
+            disp('Saving results....')
+            save([dir parse_.Results.Filename '.mat'],'-v7.3', 'H', 'A_1', 'A_2', 'lambda','alpha_bar');
     else 
         load([dir parse_.Results.Filename '.mat'])
     end
@@ -56,28 +58,24 @@ function [H, lambda] = Eigen_decomp(varargin)
     figure
         imagesc(H)
         parse_.Results.Plotsetting([0 1],1)
-        GOM2()
+        
         plot_para('Filename','Entropy_am', 'Maximize',true)
     %%
     figure
         imagesc(10*log10(abs(lambda(:,:,1))))
         parse_.Results.Plotsetting([-35 -5],1,'Colorbar_unit',"(dB)")
-        GOM2()
         plot_para('Filename','lambda_1', 'Maximize',true)
     figure
         imagesc(alpha_bar)
         parse_.Results.Plotsetting([0 60],1,'Colorbar_unit',"(deg)")
-        GOM2()
         plot_para('Filename','alpha', 'Maximize',true)
     figure
         imagesc(A_1)
         parse_.Results.Plotsetting([0 0.5],1)
-        GOM2()
         plot_para('Filename','Anisotropy1', 'Maximize',true)
     figure
         imagesc(A_2)
         parse_.Results.Plotsetting([0.6 1],1)
-        GOM2()
         plot_para('Filename','Anisotropy2', 'Maximize',true)
 end
 function ann_GOM1()
